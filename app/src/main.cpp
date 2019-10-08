62f3a53cc643938b3694cc18599ac83ef7cc0a85
Nikolas Markou <nikolasmarkou@gmail.com>
2019-10-08 16:15:06 +0300

    [setup] glog gflags dependencies setup

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <glog/logging.h>
+#include <gflags/gflags.h>
+
+DEFINE_bool(debug, true, "Show extra debug messages");
 
 int main(int argc, char** argv)
 {
-    std::cout << "working" << std::endl;
+    google::InitGoogleLogging(argv[0]);
+    gflags::ParseCommandLineFlags(&argc, &argv, true);
+    
+    LOG(INFO) << "Working";
     return 0;
 }
