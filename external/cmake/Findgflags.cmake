71f69a2d3cd22fc377391909b62396ed2f9b32c0
Nikolas Markou <nikolasmarkou@gmail.com>
2019-10-10 10:37:09 +0300

    [setup] Removed gflags and protobuf because they are already included in grpc 3rd_party

--- external/cmake/Findgflags.cmake
+++ external/cmake/Findgflags.cmake
@@ -1,5 +1,5 @@
 set(gflags_FOUND)
-set(gflags_DIR ${CMAKE_BINARY_DIR}/external/gflags/ CACHE PATH "Directory containing gflags installation" FORCE)
+set(gflags_DIR ${CMAKE_BINARY_DIR}/external/grpc/third_party/gflags/ CACHE PATH "Directory containing gflags installation" FORCE)
 set(gflags_LIBRARY_DIR ${gflags_DIR}/)
 set(gflags_INCLUDE_DIR ${gflags_DIR}/include)
 set(gflags_LIBRARY ${gflags_DIR}/libgflags_nothreads_static.a)
